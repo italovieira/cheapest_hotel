d0008d9caf54d68045653ce27baca44941d46ac0
Ítalo Vieira <italogomesv@gmail.com>
2021-11-26 20:08:10 -0300

    Add hotel model

--- hotel/models.py
+++ hotel/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import  MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Hotel(models.Model):
+    name = models.CharField(max_length=30)
+    rating = models.PositiveSmallIntegerField(
+                validators=(MinValueValidator(1), MaxValueValidator(5)))
+    weekend_regular_tax = models.FloatField()
+    weekend_reward_tax = models.FloatField()
+    weekday_regular_tax = models.FloatField()
+    weekday_reward_tax = models.FloatField()
