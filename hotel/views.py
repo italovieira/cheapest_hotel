f23025869c60cf23bd127c59f0bfb0eb1f2d36a5
Ítalo Vieira <italogomesv@gmail.com>
2021-12-11 20:07:19 -0300

    Prepare View for the /cheapest endpoint

--- hotel/views.py
+++ hotel/views.py
@@ -1,7 +1,17 @@
-from rest_framework import views
+from rest_framework import views, status
 from rest_framework.response import Response
+
+from hotel.helpers import validate_input
 
 
 class CheapestHotelView(views.APIView):
     def get(self, request):
-        return Response({})
+        try:
+            input_data = request.query_params['input']
+        except KeyError:
+            return Response({}, status.HTTP_404_NOT_FOUND)
+        else:
+            if validate_input(input_data):
+                pass
+            else:
+                return Response({}, status.HTTP_400_BAD_REQUEST)
